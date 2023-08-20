5ba794317cbadef92cff396fa9b515823e36366e
Shubham Rai <108537663+shubhamrai11@users.noreply.github.com>
2023-08-20 10:10:20 +0530

    Add files via upload

---
+++ PhotoFolio-Coding-Ninjas-main/PhotoFolio-Coding-Ninjas-main/src/App.js
@@ -0,0 +1,19 @@
+import Albumform from "./Components/AlbumForm/Albumform";
+
+
+import Navbar from "./Components/Navbar";
+import Albumslist from "./Components/AlbumsList/Albumslist"
+import Imageslist from "./Components/ImagesList/Imageslist";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    {/* <Albumform/> */}
+    <Albumslist/>
+    {/* <Imageslist/> */}
+    </>
+  );
+}
+
+export default App;
